bd154710d11b9ff4b9ed55a2b93a10be0ec6f693
AnthDavis26 <42490176+AnthDavis26@users.noreply.github.com>
2019-07-16 19:55:57 -0700

    Not doing very well. Gotta get it done.

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> CST 336 - HW3 </title>
+		<link  href="css/styles.css" rel="stylesheet" type="text/css" />
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	</head>
+	<body>
+		<h1> Test </h1>
+	</body>
+</html>
